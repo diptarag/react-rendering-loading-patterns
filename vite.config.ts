7a7764d5ada290643bad8539505f030c62aec19a
Diptarag <diptarag@gmail.com>
2024-03-08 07:29:30 +0530

    Add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import styleX from "vite-plugin-stylex";
+import styleX from 'vite-plugin-stylex';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), styleX()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          open: true,
+          filename: 'reports/bundle-analysis.html'
+        })
+      ]
+    }
+  }
 })
