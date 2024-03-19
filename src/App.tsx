86d49b735f1e145f3a79f1c2d66d906936a59417
Diptarag <diptarag@gmail.com>
2024-03-19 07:22:25 +0530

    Add basic SSR

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Layout from './pages/Layout.tsx';
 import Home from './pages/Home.tsx';
@@ -8,15 +8,13 @@ import Posts from './pages/posts';
 
 export default function App () {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route element={<Layout />}>
-          <Route path='albums/:albumsId?' element={<Albums />} />
-          <Route path='posts/:postId?' element={<Posts />} />
-          <Route path='todos' element={<ToDos />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path='albums/:albumsId?' element={<Albums />} />
+        <Route path='posts/:postId?' element={<Posts />} />
+        <Route path='todos' element={<ToDos />} />
+      </Route>
+    </Routes>
   );
 }
